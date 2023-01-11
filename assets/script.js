331456ed56c4bbda6af0db85218a7e168d7e1252
adeesha-nimsara <kabigun@gmail.com>
2023-01-11 14:19:57 +0530

    second commite

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 function myFunction() {
     var starwberry = document.getElementById("starwberry");
-    var mango = document.getElementById("mango");
+    var mango_row = document.getElementById("mango-row");
 
     new simpleParallax(starwberry,{
         delay: .6,
 	    transition: 'cubic-bezier(0,0,0,1)'
     });
 
-    new simpleParallax(mango,{
+    new simpleParallax(mango_row,{
         delay: .6,
 	    transition: 'cubic-bezier(0,0,0,1)'
     });
